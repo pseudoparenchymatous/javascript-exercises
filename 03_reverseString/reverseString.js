1f440c09d46f26c12d4c0e0ade166afcebb5ed90
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 14:07:53 +0800

    Complete function that reverses a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let finalString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        finalString += string[i];
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
