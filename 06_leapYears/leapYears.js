9dac83c2f9ab48cae9d1771fb3a900679649f161
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 15:33:58 +0800

    complete function that tests if year is leap year or not

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4) return true;
+    else if (year % 100 && year % 400) return true;
+    else return false;
 };
 
 // Do not edit below this line
