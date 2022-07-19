183f951ec980a8ae23be001a0d89525ae85e0e2a
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 13:55:16 +0800

    Complete function to pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = '';
+    for (let i = 0; i < num; i++) {
+        finalString += string;
+    }
 
+    if (!finalString) {
+        return "ERROR";
+    }
+    return finalString;
 };
 
 // Do not edit below this line
