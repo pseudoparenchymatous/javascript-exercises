197a65da814c7d411f00de5fef3a46a60b85315b
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 16:42:18 +0800

    make function that tests if a string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let palindrome = '';
+    let rawString = '';
+    for (char of string) {
+        if ((char >= 'a' && char <= 'z') || (char <= 'A' && char >= 'Z')) {
+            rawString += char;
+        }
+    }
+    for (let i = rawString.length - 1; i >= 0; i--) {
+        if ((rawString[i] >= 'a' && rawString[i] <= 'z') || (rawString[i] >= 'A' && rawString <= 'Z')) {
+            palindrome += rawString[i];
+        }
+    }
 
+    if (string === rawString) {
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
