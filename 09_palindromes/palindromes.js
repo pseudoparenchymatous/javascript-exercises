35791c4fcb9011703f8b450557e80d465924313c
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 16:45:45 +0800

    change algorithm in reversing a string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,15 +1,14 @@
 const palindromes = function (string) {
-    let palindrome = '';
     let rawString = '';
     for (char of string) {
         if ((char >= 'a' && char <= 'z') || (char <= 'A' && char >= 'Z')) {
             rawString += char;
         }
     }
+
+    let palindrome = '';
     for (let i = rawString.length - 1; i >= 0; i--) {
-        if ((rawString[i] >= 'a' && rawString[i] <= 'z') || (rawString[i] >= 'A' && rawString <= 'Z')) {
-            palindrome += rawString[i];
-        }
+        palindrome += rawString[i];
     }
 
     if (string === rawString) {
