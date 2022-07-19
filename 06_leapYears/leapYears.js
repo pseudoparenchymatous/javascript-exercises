3bcf7a10a37f2c4196e29be0b05016d4fe92e778
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 15:50:36 +0800

    fix bug not working with century years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,10 @@
-const leapYears = function(year) {
-    if (year % 4 === 0) return true;
-    else if (year % 100 === 0 && year % 400 === 0) return true;
-    else return false;
+const leapYears = function (year) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) return true;
+        else return false;
+    } else if (year % 4 === 0) {
+        return true;
+    } else return false;
 };
 
 // Do not edit below this line
