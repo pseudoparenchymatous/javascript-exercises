db888492a185c4e5fe0a15ca7964732d00ce3f4b
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 14:38:41 +0800

    finish function to pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < remove.length; j++) {
+            if (array[i] === remove[j]) array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
