6e76bc04b0b703353c721735105b4ab9175adc80
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 17:07:12 +0800

    change algorithm: store fibonacci numbers in an array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = function(num) {
-    if (num === 1 || num === 2) return 1;
+    let fib = [1, 1];
+    for (let i = 3; i <= num; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib[num - 1];
+
 };
 
 // Do not edit below this line
