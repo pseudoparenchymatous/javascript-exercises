c00374b62411b8f960fe098b840e32cbafc361fb
pseudoparenchymatous <georeysaliente@gmail.com>
2022-07-19 15:13:44 +0800

    finish function that returns sum of numbers in arguments range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+    let sum = 0;
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
