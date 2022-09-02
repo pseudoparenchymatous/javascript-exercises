d2518fca8b4d3d9fb9f4be4a554f1eb3713a3d73
pseudoparenchymatous <georeysaliente@gmail.com>
2022-09-03 04:01:43 +0800

    Use Math.pow method to power a number

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -15,11 +15,7 @@ const multiply = function(array) {
 };
 
 const power = function(num, power) {
-	let result = num;
-  for (let i = 1; i < power; i++) {
-    result *= num;
-  }
-  return result;
+  return Math.pow(num, power);
 };
 
 const factorial = function(num) {
