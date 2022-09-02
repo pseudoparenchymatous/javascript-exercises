87637a53174f01809ce56f6a2b0e1c6e85586168
pseudoparenchymatous <georeysaliente@gmail.com>
2022-09-03 04:31:10 +0800

    Return oldest person in an array using sort method

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((current, next) => {
+        const currentAge = current.yearOfDeath - current.yearOfBirth;
+        const nextAge = next.yearOfDeath - next.yearOfBirth;
+        return currentAge > nextAge ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
