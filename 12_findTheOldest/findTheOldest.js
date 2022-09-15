6ede68a7ced5ffc5a57258f98f2ad01568fb25a7
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-09-15 12:48:53 +0800

    Reset to previous commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,14 +1,10 @@
 const findTheOldest = function(people) {
-    let thisYear = new Date();
-    thisYear = thisYear.getFullYear;
-    if (people.some(person => "yearOfDeath" in person)) {
-        const peopleByAge = people.sort((current, next) => {
-            const currentAge = (("yearOfDeath" in current) ? current.yearOfDeath : thisYear) - current.yearOfBirth;
-            const nextAge = (("yearOfDeath" in next) ? next.yearOfDeath : thisYear) - next.yearOfBirth;
-            return currentAge > nextAge ? -1 : 1;
-        });
-        return oldest[0];
-    };
+    const oldest = people.sort((current, next) => {
+        const currentAge = current.yearOfDeath - current.yearOfBirth;
+        const nextAge = next.yearOfDeath - next.yearOfBirth;
+        return currentAge > nextAge ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
