40315b7579392c877d8e05f67046522519c12a4f
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2022-09-15 12:51:52 +0800

    Format indentation

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,11 @@
 const findTheOldest = function(people) {
-    const oldest = people.sort((current, next) => {
-        const currentAge = current.yearOfDeath - current.yearOfBirth;
-        const nextAge = next.yearOfDeath - next.yearOfBirth;
-        return currentAge > nextAge ? -1 : 1;
-    });
-    return oldest[0];
+
+  const oldest = people.sort((current, next) => {
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    const nextAge = next.yearOfDeath - next.yearOfBirth;
+    return currentAge > nextAge ? -1 : 1;
+  });
+  return oldest[0];
 };
 
 // Do not edit below this line
